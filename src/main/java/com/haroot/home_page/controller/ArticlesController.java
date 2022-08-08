2b308287ea87ab64fb1466df22f6393484cbcd22
sekiharuhito <sekiharuhito@sekiharuhitonoMacBook-Pro.local>
2022-08-08 18:28:34 +0900

    技術記事編集ボタン追加

--- src/main/java/com/haroot/home_page/controller/ArticlesController.java
+++ src/main/java/com/haroot/home_page/controller/ArticlesController.java
@@ -1,7 +1,5 @@
 package com.haroot.home_page.controller;
 
-import java.io.IOException;
-import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
@@ -20,13 +18,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.haroot.home_page.logic.DateLogic;
 import com.haroot.home_page.model.ArticleData;
 import com.haroot.home_page.model.IpProperties;
 import com.haroot.home_page.model.QiitaProperties;
 import com.haroot.home_page.model.SessionData;
+import com.haroot.home_page.logic.MavUtils;
 
 @Controller
 @EnableConfigurationProperties({ IpProperties.class, QiitaProperties.class })
@@ -42,58 +39,19 @@ public class ArticlesController {
 
 	@RequestMapping("articles")
 	public ModelAndView articles(ModelAndView mav) {
-		List<Map<String, Object>> articleList = jdbcT.queryForList("SELECT * FROM articles");
-		for (Map<String, Object> content : articleList) {
-			String id = content.get("id").toString();
-			content.put("id", "articles/" + id);
-		}
-		mav.addObject("articlesList", articleList);
-		mav.addObject("errStr", "");
-		mav.setViewName("contents/articles");
+		mav = MavUtils.getArticleListMav(mav, jdbcT);
 		return mav;
 	}
 
 	@RequestMapping("articles/{id}")
-	public ModelAndView list(ModelAndView mav,
-			@PathVariable("id") String id) {
-		Map<String, Object> article = jdbcT.queryForList("SELECT * FROM articles WHERE id = " + id).get(0);
-		String title = article.get("title").toString();
-		int likeCount = Integer.parseInt(article.get("like_count").toString());
-
-		// QiitaAPIから記事一覧を取得
-		try {
-			URL url = new URL("https://qiita.com/api/v2/users/"
-					+ qiitaProperties.getUser() + "/items"
-					+ "?token=" + qiitaProperties.getToken());
-			ObjectMapper mapper = new ObjectMapper();
-			JsonNode nodeList = mapper.readTree(url);
-			// 各記事情報取得
-			for (JsonNode node : nodeList) {
-				String qiitaTitle = node.get("title").toString().replaceAll("\"", "");
-				// 同じタイトルのがあったらlike数加算
-				if (qiitaTitle.equals(title)) {
-					likeCount += node.get("likes_count").asInt();
-					break;
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		mav.addObject("id", id);
-		mav.addObject("title", title);
-		mav.addObject("content", article.get("content").toString());
-		mav.addObject("like_count", likeCount);
-
-		mav.setViewName("contents/articles/template");
+	public ModelAndView list(ModelAndView mav, @PathVariable("id") String id) {
+		mav = MavUtils.getArticleMav(mav, id, qiitaProperties, jdbcT);
 		return mav;
 	}
 
 	@RequestMapping("articles/countChange/{id}/{type}")
 	@ResponseBody
-	public void countChange(
-			@PathVariable("id") String id,
-			@PathVariable("type") String type) {
+	public void countChange(@PathVariable("id") String id, @PathVariable("type") String type) {
 		String selectStr = "SELECT like_count FROM articles WHERE id=" + id;
 		List<Map<String, Object>> articleList = jdbcT.queryForList(selectStr);
 		String likeCountStr = articleList.get(0).get("like_count").toString();
@@ -112,8 +70,8 @@ public class ArticlesController {
 		return;
 	}
 
-	@RequestMapping("articles/create")
-	public ModelAndView create(ModelAndView mav, HttpServletRequest request) {
+	@RequestMapping("articles/create/{id}")
+	public ModelAndView create(ModelAndView mav, HttpServletRequest request, @PathVariable("id") String id) {
 		// 自分のみ作成できる
 		String ipAddress = request.getHeader("X-Forwarded-For");
 		if (ipAddress == null) {
@@ -121,29 +79,36 @@ public class ArticlesController {
 		}
 		if (ipAddress.equals(ipProperties.getIpAddress())) {
 			ArticleData articleData = new ArticleData();
+			// 既存記事の編集
+			if (!id.equals("-1")) {
+				Map<String, Object> article = jdbcT.queryForList("SELECT * FROM articles WHERE id=" + id).get(0);
+				String title = article.get("title").toString();
+				String content = article.get("content").toString();
+				articleData.setTitle(title);
+				articleData.setContent(content);
+			}
 			mav.addObject(articleData);
+			mav.addObject(id);
 
 			mav.setViewName("contents/articles/create");
-			// 他は戻す
 		} else {
-			List<Map<String, Object>> articleList = jdbcT.queryForList("SELECT * FROM articles");
-			for (Map<String, Object> content : articleList) {
-				String id = content.get("id").toString();
-				content.put("id", "articles/" + id);
+			// 他は戻す
+			//既存記事のページ
+			if(!id.equals("-1")) {
+				mav = MavUtils.getArticleMav(mav, id, qiitaProperties, jdbcT);
+				mav.addObject("errStr", "Sorry, you can't edit articles....");
+			}else {
+				mav = MavUtils.getArticleListMav(mav, jdbcT);
+				mav.addObject("errStr", "Sorry, you can't create articles....");
 			}
-			mav.addObject("articlesList", articleList);
-			mav.addObject("errStr", "Sorry, you can't create articles....");
-			mav.setViewName("contents/articles");
 		}
 
 		return mav;
 	}
 
-	@RequestMapping("articles/created")
-	public ModelAndView create(ModelAndView mav,
-			@ModelAttribute @Validated ArticleData articleData,
-			BindingResult errResult,
-			HttpServletRequest request,
+	@RequestMapping("articles/created/{id}")
+	public ModelAndView create(ModelAndView mav, @ModelAttribute @Validated ArticleData articleData,
+			@PathVariable("id") String id, BindingResult errResult, HttpServletRequest request,
 			HttpServletResponse response) {
 		// エラーがあれば戻る
 		if (errResult.hasErrors()) {
@@ -156,9 +121,15 @@ public class ArticlesController {
 		String title = articleData.getTitle();
 		String content = articleData.getContent();
 
-		// 記事情報をdbにinsert
-		String sqlStr = "INSERT INTO articles(title, content, create_date) VALUES(?,?,?)";
-		jdbcT.update(sqlStr, title, content, dateStr);
+		// 新規記事
+		if (id.equals("-1")) {
+			String sqlStr = "INSERT INTO articles(title, content, create_date) VALUES(?,?,?)";
+			jdbcT.update(sqlStr, title, content, dateStr);
+			// 既存記事
+		} else {
+			String sqlStr = "UPDATE articles SET title=?, content=? WHERE id=" + id;
+			jdbcT.update(sqlStr, title, content);
+		}
 
 		mav.setViewName("contents/articles/created");
 
---
+++ src/main/java/com/haroot/home_page/logic/MavUtils.java
@@ -0,0 +1,60 @@
+package com.haroot.home_page.logic;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.haroot.home_page.model.QiitaProperties;
+
+public class MavUtils {
+	public static ModelAndView getArticleMav(ModelAndView mav, String id, QiitaProperties qiitaProperties, JdbcTemplate jdbcT) {
+		Map<String, Object> article = jdbcT.queryForList("SELECT * FROM articles WHERE id = " + id).get(0);
+		String title = article.get("title").toString();
+		int likeCount = Integer.parseInt(article.get("like_count").toString());
+
+		// QiitaAPIから記事一覧を取得
+		try {
+			URL url = new URL("https://qiita.com/api/v2/users/" + qiitaProperties.getUser() + "/items" + "?token="
+					+ qiitaProperties.getToken());
+			ObjectMapper mapper = new ObjectMapper();
+			JsonNode nodeList = mapper.readTree(url);
+			// 各記事情報取得
+			for (JsonNode node : nodeList) {
+				String qiitaTitle = node.get("title").toString().replaceAll("\"", "");
+				// 同じタイトルのがあったらlike数加算
+				if (qiitaTitle.equals(title)) {
+					likeCount += node.get("likes_count").asInt();
+					break;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		mav.addObject("id", id);
+		mav.addObject("title", title);
+		mav.addObject("content", article.get("content").toString());
+		mav.addObject("like_count", likeCount);
+
+		mav.setViewName("contents/articles/template");
+		return mav;
+	}
+	
+	public static ModelAndView getArticleListMav(ModelAndView mav, JdbcTemplate jdbcT) {
+		List<Map<String, Object>> articleList = jdbcT.queryForList("SELECT * FROM articles");
+		for (Map<String, Object> content : articleList) {
+			String id = content.get("id").toString();
+			content.put("id", id);
+		}
+		mav.addObject("articlesList", articleList);
+		mav.addObject("errStr", "");
+		mav.setViewName("contents/articles");
+		return mav;
+	}
+}
