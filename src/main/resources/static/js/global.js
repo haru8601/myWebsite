bd30dac626c2209e68ae5c46fdeb3d84fe4fbfb0
sekiharuhito <haroot.net@gmail.com>
2026-03-26 08:24:36 +0900

    feat: トップ画面のキーボード入力受け付け

---
+++ src/main/resources/static/js/global.js
@@ -0,0 +1,20 @@
+/**
+ * not-nullな要素に対するラッパー関数
+ * @param {HTMLElement | null} element
+ * @returns {HTMLElement} not-nullな要素
+ */
+const _notNullWrapper = (element) => {
+  if (!element) {
+    throw new Error("element is NOT found.");
+  }
+  return element;
+};
+
+/**
+ * 要素を別要素内に移動
+ * @param target {HTMLElement}
+ * @param to {HTMLElement}
+ */
+const _moveElement = (target, to) => {
+  to.appendChild(target);
+};
--- src/main/resources/static/js/top.js
+++ src/main/resources/static/js/top.js
@@ -2,27 +2,44 @@
  * @type {NodeListOf<HTMLElement>}
  */
 const chooseItems = document.querySelectorAll(".hr-choose-item");
-/**
- * @type {HTMLElement | null}
- */
-const chooseIcon = document.querySelector(".hr-choose-icon");
 
 /**
- * 要素を別要素内に移動
- * @param target {HTMLElement}
- * @param to {HTMLElement}
+ * @type {HTMLElement}
  */
-const _moveElement = (target, to) => {
-  to.appendChild(target);
-};
+const chooseIcon = _notNullWrapper(document.querySelector(".hr-choose-icon"));
 
 /**
  * 選択セクションホバー時の処理
  */
 chooseItems.forEach((item) => {
   item.addEventListener("mouseenter", () => {
-    if (chooseIcon) {
-      _moveElement(chooseIcon, item);
-    }
+    _moveElement(chooseIcon, item);
   });
+});
+
+const CHOOSE_ITEM_MIN_INDEX = 0;
+const CHOOSE_ITEM_MAX_INDEX = 999;
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowDown") {
+    const chooseIndex = parseInt(
+      chooseIcon.parentElement?.dataset.hrChooseIndex ??
+        CHOOSE_ITEM_MAX_INDEX.toString(),
+    );
+    // アイコンの親要素が最後のindexでなければ
+    if (chooseIndex < chooseItems.length - 1) {
+      _moveElement(chooseIcon, chooseItems[chooseIndex + 1]);
+    }
+  } else if (e.key == "ArrowUp") {
+    const chooseIndex = parseInt(
+      chooseIcon.parentElement?.dataset.hrChooseIndex ??
+        CHOOSE_ITEM_MIN_INDEX.toString(),
+    );
+    // アイコンの親要素が最初のindexでなければ
+    if (chooseIndex > CHOOSE_ITEM_MIN_INDEX) {
+      _moveElement(chooseIcon, chooseItems[chooseIndex - 1]);
+    }
+  } else {
+    // TODO: ヒントを表示
+  }
 });
