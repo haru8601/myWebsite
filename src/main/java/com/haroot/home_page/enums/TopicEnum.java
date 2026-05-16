690ee6de57aee9e67ded29b0dedce8cc8db46236
sekiharuhito <haroot.net@gmail.com>
2026-05-16 19:13:15 +0900

    feat: metaタグの修正

--- src/main/java/com/haroot/home_page/enums/TopicEnum.java
+++ src/main/java/com/haroot/home_page/enums/TopicEnum.java
@@ -4,8 +4,8 @@ import lombok.Getter;
 
 @Getter
 public enum TopicEnum {
-  ABOUT("ABOUT", "経歴", "about", "harootとは...?", "icon/about.svg"),
-  WORK("WORK", "ワークス", "work", "個人で製作した作品一覧です。", "icon/work.svg"),
+  ABOUT("ABOUT", "harootとは", "about", "harootの概要、スキル、経歴の紹介ページです。", "icon/about.svg"),
+  WORK("WORK", "作品一覧", "work", "haroot個人で製作した作品一覧です。主にアプリやWebで遊べるゲームで構成されています。", "icon/work.svg"),
   ARTICLES("ARTICLES", "技術記事", "articles", "IT周りの自分用にまとめた記事一覧です。", "icon/articles.svg"),
   CONTACT("CONTACT", "お問い合わせ", "contact", "お問い合わせフォームはこちら。", "icon/contact.svg");
 
