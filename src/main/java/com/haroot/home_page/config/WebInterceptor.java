58860c6b6b9f37a3ff5fac070ac98f360d97af5e
sekiharuhito <haroot.net@gmail.com>
2026-06-06 22:45:06 +0900

    feat: 古いエンドポイントをしばらく残しておく

--- src/main/java/com/haroot/home_page/config/WebInterceptor.java
+++ src/main/java/com/haroot/home_page/config/WebInterceptor.java
@@ -3,12 +3,14 @@ package com.haroot.home_page.config;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class WebInterceptor implements HandlerInterceptor {
+
     /**
      * Controllerの処理終了後のインターセプター
      */
@@ -18,8 +20,13 @@ public class WebInterceptor implements HandlerInterceptor {
             HttpServletResponse response,
             Object handler,
             ModelAndView modelAndView) throws Exception {
-        if (modelAndView != null) {
+        // NOTE: リダイレクトだと、デフォルトでモデルをクエリパラメータに付与してしまうため除外する
+        if (modelAndView != null && !isRedirectView(modelAndView)) {
             modelAndView.addObject("uri", request.getRequestURI());
         }
+    }
+
+    private boolean isRedirectView(ModelAndView modelAndView) {
+        return modelAndView != null && modelAndView.getView() instanceof RedirectView;
     }
 }
