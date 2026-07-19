758f85fb08cc2fc3d522c64c231cf740d0957372
sekiharuhito <haroot.net@gmail.com>
2026-07-20 02:34:07 +0900

    fix: 404時のステータスコード修正

--- src/main/java/com/haroot/home_page/exceptionhandler/HarootExceptionHandler.java
+++ src/main/java/com/haroot/home_page/exceptionhandler/HarootExceptionHandler.java
@@ -1,14 +1,18 @@
 package com.haroot.home_page.exceptionhandler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.haroot.home_page.exception.HarootNotFoundException;
 
+import jakarta.servlet.http.HttpServletResponse;
+
 @ControllerAdvice
 public class HarootExceptionHandler {
   @ExceptionHandler(HarootNotFoundException.class)
-  public String handleHarootNotFoundException(HarootNotFoundException ex) {
+  public String handleHarootNotFoundException(HarootNotFoundException ex, HttpServletResponse response) {
+    response.setStatus(HttpStatus.NOT_FOUND.value());
     return "/error/404";
   }
 }
