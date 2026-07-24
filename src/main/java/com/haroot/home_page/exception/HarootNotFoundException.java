615165c596425a763bbb1b21beca8057da6d2779
sekiharuhito <haroot.net@gmail.com>
2026-07-25 01:54:44 +0900

    fix: 404であることを明示

--- src/main/java/com/haroot/home_page/exception/HarootNotFoundException.java
+++ src/main/java/com/haroot/home_page/exception/HarootNotFoundException.java
@@ -1,5 +1,9 @@
 package com.haroot.home_page.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class HarootNotFoundException extends RuntimeException {
 
   private static final long serialVersionUID = 1L;
